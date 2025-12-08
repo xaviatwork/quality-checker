92fb000cd826d8a54c8e8c53ea419a14088ea9a4
Xavi Aznar <xavier.aznar@seat.es>
2025-12-08 14:28:57 +0000

    refactor

--- checker.sh
+++ checker.sh
@@ -1,113 +1,134 @@
 #!/usr/bin/env bash
-
-print_header() {
-    local repository msg
-    repository="$1"
-    msg=$(printf "ShellCheck findings for $repository\n")
-    
-    echo "$msg"
-    print_line "${#msg}"
-}
-
-print_total() {
-    msg=$(printf "%-8s %5d\n" "TOTAL" "$total")
-    print_line
-    echo "$msg"
-}
-
-print_line() {
-    local length
-    length=$(( 8 + 1 + 5 + 1 + 3 + 1 + 1 + 2 + 1 + 100)) # Length of the bar and other fields
-    for (( i=0; i< length; i++)); do
-        printf "-"
+main_args() {
+    report_raw=$(mktemp -t shellcheck.XXX)
+    report=$(mktemp -t shellcheck.XXX)
+    info=0
+    warning=0
+    error=0
+    bw='false'
+    while [[ $# -gt 0 ]]
+    do
+        key="$1"
+        case "$key" in
+            --repo)
+                repo=${2:?$1 cannot be empty}
+                shift 2
+            ;;
+            --no-color)
+                bw='true'
+                shift
+            ;;
+            *) shift;;
+        esac
     done
-    echo ""
+    repo=${repo:?'--repo' is mandatory}
+
+    if [[ $bw == 'false' ]];then
+        color_auto='\e[0m'
+        color_red='\e[0;31m'
+        color_yellow='\e[1;33m'
+        color_blue='\e[0;34m'
+    fi
+}
+
+main() {
+    local repo total info warning error
+    main_args "$@"
+
+    if ! found_sh_files "$repo"; then
+        exit 1
+    fi
+
+    shellchecker "$repo"
+    total=$(jq '. | length' $report)
+    # Get ShellCheck findings
+    if [[ $total -ne 0 ]]; then
+        info=$(jq '[ select( .[].level == "info" ) ] | length' "$report")
+        warning=$(jq '[ select( .[].level == "warning" )] | length' "$report")
+        error=$(jq '[ select( .[].level == "error" )] | length' "$report")
+    fi
+
+    spacer=" "
+    print_header $repo $total
+    print_bar $info $total "info"
+    print_bar $warning $total "warning"
+    print_bar $error $total "error"
+}
+
+found_sh_files() {
+    # Check if target location contain *.sh files
+    local repository
+
+    repository="$1"
+    if [[ ! -d "$repository" ]]; then
+        echo >&2 "[ERROR] Repository '$repository' cannot be found"
+        return 1
+    fi
+    if ! find "$repository"/*.sh -prune -path "*/.devcontainer/*" -path "*/git/*"> /dev/null ; then 
+        echo >&2 "[WARNING] Repository '$repository' does not contains *.sh files"
+        return 1
+    fi
+}
+
+shellchecker() {
+    local repository
+    repository="$1"
+    while read -r file; do
+        shellcheck "$file" --format=json >> $report_raw
+    done < <( find "$repository"/*.sh )
+    # Join all individual reports into one 
+    jq '.[] | {"level": .level}' $report_raw | jq -s > $report
 }
 
 percentage() {
     local total value
     value=$1
     total=$2
-
     echo $(( value * 100 / total ))
 }
 
+print_header() {
+    local repository msg
+    repository="$1"
+    msg=$(printf "ShellCheck findings for '$repository'\n")
+    
+    spacer=" "
+    printf "\n%8s ├ %-100s┤ %s\n" "$spacer" "$msg" "TOTAL: $total"
+}
+
+
 print_bar() {
-    local value total percent label
-    local COLOR_AUTO COLOR_RED COLOR_YELLOW COLOR_BLUE
+    local value total label percent
     value=$1
     total=$2
     label="$3"
 
-    COLOR_AUTO='\e[0m'
-    COLOR_RED='\e[0;31m'
-    COLOR_YELLOW='\e[1;33m'
-    COLOR_BLUE='\e[0;34m'
-
     case $label in
-        "info"   ) color=$COLOR_BLUE;;
-        "warning") color=$COLOR_YELLOW;;
-        "error"  ) color=$COLOR_RED;;
-        *        ) color=$COLOR_AUTO ;;
+        # "info"   ) color=$color_blue; bar_char="░";;
+        "info"   ) color=$color_blue; bar_char="▒";;
+        "warning") color=$color_yellow; bar_char="▓";;
+        "error"  ) color=$color_red; bar_char="█";;
+        *        ) color=$color_auto ;;
     esac
+
+    if [[ $bw == 'true' ]]; then
+        color=''
+    fi
 
     if (( total != 0 )); then
         percent=$(percentage "$value" "$total")
     fi
 
-    printf "%-8s %5d (%3d %%) " "$(echo "$label" | awk '{print toupper($0)}')" "$value" "$percent"
-    printf "%b" "$color"
+    bar=""
     for (( i=0;i < percent; ++i )) ; do
-        printf "█"
+        bar="${bar}${bar_char}"
     done
-    printf "%b\n" "$COLOR_AUTO"
-}
-
-main() {
-    local repository raw_report_file report_file
-    local total info warning error
-    
-    raw_report_file="/tmp/raw_output.json"
-    report_file="/tmp/repost.json"
-    repository="$1"
-
-    # Check if target location contain *.sh files
-    if ! find "$repository"/*.sh  &> /dev/null ; then 
-        echo "[WARNING] Repository '$repository' does not contains *.sh files"
-        return
-    fi
-
-    # Clean Up
-    if [[ -f $raw_report_file ]]; then rm $raw_report_file; fi
-    if [[ -f $report_file ]]; then rm $report_file; fi
-
-    # Generate raw report using ShellCheck
-    for f in "$repository"/*.sh; do
-        shellcheck "$f" --format=json | jq . >> "$raw_report_file"
+    for (( i=percent;i <= 100; ++i )) ; do
+        bar="${bar}·"
     done
-    # Filter raw report using Jq
-    jq '.[] | {"level": .level}' "$raw_report_file" | jq -s > "$report_file"
-
-    # Count findings
-    total=$(jq 'length' "$report_file")
-    if (( total != 0 )); then
-        info=$(jq ' [ select( .[].level == "info" )] | length' "$report_file")
-        warning=$(jq ' [ select( .[].level == "warning" )] | length' "$report_file")
-        error=$(jq ' [ select( .[].level == "error" )] | length' "$report_file")
-    else
-        info=0
-        warning=0
-        error=0
-    fi
-
-    # Print Report
-    print_header "$repository"
-    print_bar $info $total "info"
-    print_bar $warning $total "warning"
-    print_bar $error $total "error"
-    print_total
+    printf "%8s ├%b%100s%b┤ %3d %% (%s)\n" "$(echo "$label" | awk '{print toupper($0)}')" "$color" "$bar" "$color_auto" "$percent" "$value"
 }
 
 # ----
 
-main "$1"
+main "$@"
