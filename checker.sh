71978f5d1088d07954b0ce65180020799bbe8cb0
Xavi Aznar <xavier.aznar@seat.es>
2025-12-09 19:25:37 +0100

    refactor: declare variables as local explicitly

--- checker.sh
+++ checker.sh
@@ -1,5 +1,54 @@
 #!/usr/bin/env bash
+
+main() {
+    local repo
+    local style info warning error
+    local report report_raw format csv_separator csv_ouput
+    local bw color_auto color_red color_yellow color_blue
+    local total qscore
+    main_args "$@"
+
+    if ! found_sh_files "$repo"; then
+        exit 1
+    fi
+
+    shellchecker "$repo"
+    total=$(jq '. | length' "$report")
+    # Get ShellCheck findings
+    if [[ $total -ne 0 ]]; then
+        style=$(jq '[ select( .[].level == "style" ) ] | length' "$report")
+        info=$(jq '[ select( .[].level == "info" ) ] | length' "$report")
+        warning=$(jq '[ select( .[].level == "warning" )] | length' "$report")
+        error=$(jq '[ select( .[].level == "error" )] | length' "$report")
+    fi
+
+    qscore=$(( 0 - style - 2*info - 4*warning - 8*error ))
+    
+    case $format in
+        csv)
+            csv_separator=';'
+            csv_ouput=$(mktemp -t csv.XXXXX)
+            (
+                echo "Repository $csv_separator style $csv_separator Info $csv_separator Warning $csv_separator Error $csv_separator Total $csv_separator Qscore"
+                echo "$repo $csv_separator $style $csv_separator $info $csv_separator $warning $csv_separator $error $csv_separator $total $csv_separator  $qscore"
+            ) | tee "$csv_ouput"
+        ;;
+        qscore)
+            echo "$qscore"
+        ;;
+        *)
+            # Default: Histogram
+            print_header "$repo" "$total"
+            print_bar "$style" "$total" "style"
+            print_bar "$info" "$total" "info"
+            print_bar "$warning" "$total" "warning"
+            print_bar "$error" "$total" "error"
+        ;;
+    esac
+}
+
 main_args() {
+    # Default values
     report_raw=$(mktemp -t shellcheck.XXX)
     report=$(mktemp -t shellcheck.XXX)
     style=0
@@ -30,57 +79,16 @@ main_args() {
             *) shift;;
         esac
     done
+    # Check required variables
     repo=${repo:?'--repo' is mandatory}
 
+    # Set colors
     if [[ $bw == 'false' ]];then
         color_auto='\e[0m'
         color_red='\e[0;31m'
         color_yellow='\e[1;33m'
         color_blue='\e[0;34m'
     fi
-}
-
-main() {
-    local repo total info warning error format total
-    main_args "$@"
-
-    if ! found_sh_files "$repo"; then
-        exit 1
-    fi
-
-    shellchecker "$repo"
-    total=$(jq '. | length' "$report")
-    # Get ShellCheck findings
-    if [[ $total -ne 0 ]]; then
-        style=$(jq '[ select( .[].level == "style" ) ] | length' "$report")
-        info=$(jq '[ select( .[].level == "info" ) ] | length' "$report")
-        warning=$(jq '[ select( .[].level == "warning" )] | length' "$report")
-        error=$(jq '[ select( .[].level == "error" )] | length' "$report")
-    fi
-
-    qscore=$(( 0 - style - 2*info - 4*warning - 8*error ))
-    
-    case $format in
-        csv)
-            csv_separator=';'
-            out=$(mktemp -t csv.XXXXX)
-            (
-                echo "Repository $csv_separator Info $csv_separator Warning $csv_separator Error $csv_separator Total $csv_separator Qscore"
-                echo "$repo $csv_separator $info $csv_separator $warning $csv_separator $error $csv_separator $total $csv_separator  $qscore"
-            ) | tee "$out"
-        ;;
-        qscore)
-            echo "$qscore"
-        ;;
-        *)
-            # Default: Histogram
-            print_header "$repo" "$total"
-            print_bar "$style" "$total" "style"
-            print_bar "$info" "$total" "info"
-            print_bar "$warning" "$total" "warning"
-            print_bar "$error" "$total" "error"
-        ;;
-    esac
 }
 
 found_sh_files() {
@@ -102,7 +110,7 @@ shellchecker() {
     local repository
     repository="$1"
     while read -r file; do
-        shellcheck "$file" --format=json >> "$report_raw"
+        shellcheck "$file" --format=json >> $report_raw
     done < <( find "$repository"/*.sh )
     # Join all individual reports into one 
     jq '.[] | {"level": .level}' "$report_raw" | jq -s > "$report"
@@ -126,15 +134,15 @@ print_header() {
     printf "\n%12s %-101s %b %s %b\n" "QSCORE: $qscore" "$msg" "$ok" "TOTAL: $total" "$color_auto"
 }
 
-
 print_bar() {
     local value total label percent
+    local bar bar_char
     value=$1
     total=$2
     label="$3"
 
     case $label in
-        "style"   ) color=$color_auto; bar_char="░";;
+        "style"  ) color=$color_auto; bar_char="░";;
         "info"   ) color=$color_blue; bar_char="▒";;
         "warning") color=$color_yellow; bar_char="▓";;
         "error"  ) color=$color_red; bar_char="█";;
@@ -159,6 +167,8 @@ print_bar() {
     printf "%12s ├%b%100s%b┤ %3d %% (%s)\n" "$(echo "$label" | awk '{print toupper($0)}')" "$color" "$bar" "$color_auto" "$percent" "$value"
 }
 
+# Check for dependencies
+which jq > /dev/null         || { echo "'jq' was not found in the \$PATH" ; exit 1; }
+which shellcheck > /dev/null || { echo "'shellcheck' was not found in the \$PATH" ; exit 1; } 
 # ----
-
 main "$@"
